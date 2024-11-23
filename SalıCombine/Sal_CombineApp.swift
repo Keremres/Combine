f1fa954ae5d77b91f141de2cb1eb5a8471ff1582
Keremres <keremre-16@hotmail.com>
2024-11-23 11:33:54 +0300

    Initial Commit

---
+++ SalıCombine/Sal_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sal_CombineApp.swift
+//  SalıCombine
+//
+//  Created by Kerem RESNENLİ on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sal_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
